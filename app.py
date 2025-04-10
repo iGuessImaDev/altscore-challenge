bfd0638f49c21edd97c0773ea6ef1e6bd14cccdf
Sebastián Gallardo <iguesimadev@gmail.com>
2025-04-10 01:02:29 -0400

    memory between requests

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, render_template_string, session
+from flask import Flask, jsonify, render_template_string
 import random
 
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
 
-system_codes = {
+SYSTEM_CODES = {
     "navigation": "NAV-01",
     "communications": "COM-02",
     "life_support": "LIFE-03",
@@ -12,29 +12,28 @@ system_codes = {
     "deflector_shield": "SHLD-05"
 }
 
+damaged_system = None  # memory between requests
+
 @app.route('/status', methods=['GET'])
-def get_status():
-    damaged_system = random.choice(list(system_codes.keys()))
-    session['damaged_system'] = damaged_system
+def status():
+    global damaged_system
+    damaged_system = random.choice(list(SYSTEM_CODES.keys()))
     return jsonify({"damaged_system": damaged_system})
 
 @app.route('/repair-bay', methods=['GET'])
 def repair_bay():
-    damaged_system = session.get('damaged_system')
-    repair_code = system_codes.get(damaged_system, "UNKNOWN")
-    
-    html_content = f"""
+    global damaged_system
+    code = SYSTEM_CODES.get(damaged_system, "UNKNOWN")
+    html = f"""
     <!DOCTYPE html>
     <html>
-    <head>
-        <title>Repair</title>
-    </head>
+    <head><title>Repair</title></head>
     <body>
-    <div class="anchor-point">{repair_code}</div>
+        <div class="anchor-point">{code}</div>
     </body>
     </html>
     """
-    return render_template_string(html_content)
+    return render_template_string(html)
 
 @app.route('/teapot', methods=['POST'])
 def teapot():
